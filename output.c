c73fa5be88f6f262218ad8da1c11c9473526f618
Vinícius Aiala <vinicius_aiala@outlook.com>
2021-09-22 20:02:11 -0300

    arquivo em c compilado

---
+++ output.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+void main() {
+   int i;
+   int soma;
+   int somaFor;
+   int n;
+   int verd;
+
+   n = 100;
+   soma = 0;
+   i = 0;
+   verd = 2;
+   while ((i < n) && !!verd) {
+      if ( ((i % 2) == (0 + ((0 * 0) / 1))) ) { 
+         soma = (--soma + (i * i));
+      }
+      i = (i + 1);
+   }
+   somaFor = 0;
+   for (int k = 0; k < 100; ++k) {
+      if ( ((k % 2) == 0) ) { 
+         somaFor = (somaFor + (k * k));
+      }
+   }
+   printf("%d\n", soma);
+   printf("%d\n", somaFor);
+}
